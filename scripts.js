0362fd081e6024b7da18e850f133288012901986
Maxen <240841057+6maxen@users.noreply.github.com>
2026-04-27 15:07:18 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,42 @@
+let cookie = document.getElementById("cookie")
+let counter = document.getElementById("counter")
+let goal = document.getElementById("goal")
+let goalText = document.getElementById("goal-text")
+const clickSound = new Audio('click.mp3')
+
+cookie.addEventListener('click', function(e) {
+    clickSound.currentTime = 0  // resets so rapid clicks work
+    clickSound.play()
+    counter.textContent++
+    
+    cookie.style.transition = 'transform .1s ease'
+    cookie.style.transform = 'scale(1.02) rotate(0deg)'
+    
+    setTimeout(() => {
+        cookie.style.transform = 'scale(.9)'
+    }, 100)
+    if (counter.textContent >= 300) {
+        goalText.textContent = "."
+    } else if (counter.textContent >= 255) {
+        goalText.textContent = "game's actually over, go away!"
+    } else if (counter.textContent >= 251) {
+        goalText.textContent = "dang it, you must've double clicked. sorry about that."
+    } else if (counter.textContent == 250) {
+        counter.textContent = 251
+        goalText.textContent = "dang it, you must've double clicked. Sorry about that."
+    } else if (counter.textContent >= 150) {
+        goalText.textContent = "okay fine, if you hit exactly '250', I'll give you a giftcard for 10$ uber eats."
+    } else if (counter.textContent >= 125) {
+        goalText.textContent = "there's no prizes or anything..."
+    } else if (counter.textContent >= 105) {
+        goalText.textContent = "were you expecting something at 100?"
+    } else if (counter.textContent >= 75) {
+        goalText.textContent = "well, I'm going to run some errands, brb."
+    } else if (counter.textContent >= 50) {
+        goalText.textContent = "the game's over fyi"
+    } else if (counter.textContent >= 33) {
+        goalText.textContent = "why are you still clicking?"
+    } else if (counter.textContent >= Number(goal.textContent)) {
+        goalText.textContent = "congrats!"
+    }
+})
